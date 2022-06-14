75d71e8b148f3908580392c4030cdd04863219b9
edochik <edoch@yandex.ru>
2022-06-15 01:42:55 +0300

    add method1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] newArray = { "Hello", "2", "word", ":-)" };
+
+int CountIndexArrayLessThree(string[] array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            j += 0;
+        }
+    }
+    return j;
+}
