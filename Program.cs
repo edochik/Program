a2bfb04a9493aa429d30e2970ab47d5682423663
edochik <edoch@yandex.ru>
2022-06-15 03:46:49 +0300

    add cod

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿string[] newArray = { "Hello", "2", "word", ":-)" };
+﻿Console.WriteLine("Ввод массива с клавиатуры? Нажми 1| Выбрать массив. Нажми 2");
+Console.WriteLine();
+int b = Convert.ToInt16(Console.ReadLine());
+
+string[] arrayFirst = { "Hello", "2", "word", ":-)" };
+string[] arraySecond = { "1234", "1567", "-2", "computer science" };
+string[] arrayThird = { "Russia", "Denmark", "Kazan" };
 
 int CountIndexArrayLessThree(string[] array)
 {
@@ -13,7 +19,7 @@ int CountIndexArrayLessThree(string[] array)
     return j;
 }
 
-void ReplaceOldArrayNewArray(string[] array, int count)
+void ReplaceOldArrayNewArray(ref string[] array, int count)
 {
     string[] newArray = new string[count];
     int j = 0;
@@ -27,3 +33,79 @@ void ReplaceOldArrayNewArray(string[] array, int count)
     }
     array = newArray;
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+}
+
+
+if (b == 1)
+{
+
+    Console.WriteLine("Введите кол-во строк(цифры): ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    string[] UserArray = new string[n];
+    void EmptyArray(string[] array)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+            UserArray[i] = Console.ReadLine();
+        }
+    }
+    EmptyArray(UserArray);
+    Console.WriteLine();
+    Console.WriteLine("Вывод:");
+    ReplaceOldArrayNewArray(ref UserArray, CountIndexArrayLessThree(UserArray));
+    PrintArray(UserArray);
+
+}
+
+if (b == 2)
+{
+    Console.WriteLine("выберите массив 1, 2, 3:");
+    Console.Write("1 = ");
+    PrintArray(arrayFirst);
+    Console.WriteLine();
+    Console.Write("2 = ");
+    PrintArray(arraySecond);
+    Console.WriteLine();
+    Console.Write("3 = ");
+    PrintArray(arrayThird);
+    Console.WriteLine();
+    int c = Convert.ToInt32(Console.ReadLine());
+    if (c == 1)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Вывод:");
+        ReplaceOldArrayNewArray(ref arrayFirst, CountIndexArrayLessThree(arrayFirst));
+        PrintArray(arrayFirst);
+    }
+    if (c == 2)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Вывод:");
+        ReplaceOldArrayNewArray(ref arraySecond, CountIndexArrayLessThree(arraySecond));
+        PrintArray(arraySecond);
+    }
+    if (c == 3)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Вывод:");
+        ReplaceOldArrayNewArray(ref arrayThird, CountIndexArrayLessThree(arrayThird));
+        PrintArray(arrayThird);
+    }
+    else
+    {
+        Console.Write("Не правильный ввод");
+    }
+}
+if (b > 2 || b < 1)
+{
+    Console.Write(" Перезапусти программу, и введи 1 или 2");
+}
+
