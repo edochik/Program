7692d935be1063f8aebcbeb039c6acd41da3b914
edochik <edoch@yandex.ru>
2022-06-15 03:34:40 +0300

    add method2

--- Program.cs
+++ Program.cs
@@ -12,3 +12,18 @@ int CountIndexArrayLessThree(string[] array)
     }
     return j;
 }
+
+void ReplaceOldArrayNewArray(string[] array, int count)
+{
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    array = newArray;
+}
